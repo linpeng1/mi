981d032cb0710f1f5697ecfff9ff02e8f2df4c31
linpeng1 <30601207+linpeng1@users.noreply.github.com>
2018-11-05 23:42:27 +0800

    Add files via upload
    mi

---
+++ Mi.java
@@ -0,0 +1,42 @@
+
+public class Mi {
+	public static void main(String[] args) {
+		char[] s1 = getResult("ammimiib","mi");
+		System.out.println(s1);
+	}
+	
+	public static char[] getResult(String str,String delSubStr){
+		char[] s1 = str.toCharArray();
+		char[] s2 = delSubStr.toCharArray();
+		int i = 0;
+		int j = 0;
+		int count = 0;
+		while (i < s1.length) {
+			if (count >= s2.length && count > 0) {
+				j = i - count;
+				while (i < s1.length) {
+					s1[j++] = s1[i++];
+				}
+				while(j< s1.length){
+					s1[j++] = 0;
+				}
+				i = 0;
+				count = 0;
+				continue;
+			}
+			if (s1[i] == s2[count]) {
+				count++;
+			} else if (s1[i] == s2[0]) {
+				count = 1;
+			} else {
+				count = 0;
+			}
+			i++;
+		}
+		if (count >= s2.length && count > 0) {
+			s1[i - count] = 0;
+		} 
+		return s1;
+	}
+
+}
